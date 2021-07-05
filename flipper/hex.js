58098cc4431a1ccc913f4f34326c8e0672519be5
berkiyo <berk@tekbyte.net>
2021-07-05 13:54:02 +1000

    Few changes from Textastic (demo)

--- flipper/hex.js
+++ flipper/hex.js
@@ -3,6 +3,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+// The button listener, we need this. :)
 btn.addEventListener('click', function(){
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
@@ -13,6 +14,7 @@ btn.addEventListener('click', function(){
     document.body.style.backgroundColor = hexColor;
 });
 
+// Multiply the random number by the length of the "hex" which is 16.
 function getRandomNumber() {
     return Math.floor(Math.random() * hex.length)
 }
